8f906b5a799a11b8c7f568b01cc8efddcb3d317b
Santiago Bassani <sbassani@fi.uba.ar>
2025-07-08 13:36:24 -0300

    Supabase bd

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,10 +1,16 @@
 const { Pool } = require("pg");
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+
 
 const dbClient = new Pool({
-  user: "postgres",
+  /* user: "postgres",
   password: "postgres",
   port: 5432,
-  database: "tiendaPC"
+  database: "tiendaPC" */
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = dbClient;
